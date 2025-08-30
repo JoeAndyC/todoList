2efc24ba1958c2d2e331f03d8c14cf779cb0e7c5
JoeAndyC <joelcuenca.jc@gmail.com>
2025-08-30 15:22:47 -0300

    Se quito el boton de borrar, que se generaba con cuando se agregaba una tarea y se colocó un checkbox

--- scripts/main.js
+++ scripts/main.js
@@ -5,26 +5,25 @@ const enterTask = document.querySelector('#enterTask');
 addTask.addEventListener('click', () => {
   let inputTask = document.createElement('input');
   let labelInput = document.createElement('label');
-  
+  let checkInput = document.createElement('input');
+
+  //* Input to mark when the task it is done
   inputTask.type = 'radio';
   inputTask.class = 'task';
   inputTask.name = 'valueTask';
   
   labelInput.textContent = enterTask.value;
   labelInput.name = 'valueTask';
+
+  //* Input to mark the task we want to delete
+  checkInput.type = 'checkbox';
+  checkInput.class = 'checkDelete';
+  checkInput.name = 'deleteChecking';  
   
-  let btnDelete = document.createElement('button');
-  btnDelete.class = 'delete';
-  btnDelete.textContent = 'Borrar';
-  
- //TODO Button function. Think how work with the input
-  
-  enterTask.textContent = '';
-  enterTask.placeholder = 'Ingrese su tarea'; 
-  
+  //enterTask.textContent = '';
+  //enterTask.placeholder = 'Ingrese su tarea...';  
+    
   taskDiv.appendChild(inputTask);
   taskDiv.appendChild(labelInput);
-  taskDiv.appendChild(btnDelete);
-  
-  
+  taskDiv.appendChild(checkInput);  
 });
