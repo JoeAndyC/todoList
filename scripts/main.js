4e2b91c5eec83f15305f149d0c46581c8ff1568c
JoeAndyC <joelcuenca.jc@gmail.com>
2025-10-18 16:29:16 -0300

    Se agrego la funcion para el botón añadir, para que cree los elementos

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,25 @@ const newTask = document.querySelector('.newTask');
 const addTaskButton =  document.querySelector('buttonAddTask');
 const taskList = document.querySelector('.taskList');
 
+const task = [];
 
 while(true) {
   try {
     if (newTask.value !== '' || newTask.value !== undefined || newTask.value !== null) {
       addTaskButton.addEventListener('clicl', => {
         const task = document.createElement('div');
+        const radioModInput = document.createElement('div');
         const pTask = document.createElement('p');
         const deleteButton = document.createElement('button');
         
-      })  
+        // Enter the class for each element
+        task.className = 'task';
+        radioModInput.className = 'radioModInput';
+        pTask.className = 'task';
+        deleteButton.className =  'deleteButton';
+
+        // Enter the task in the array
+      });  
     }
   }
 }
