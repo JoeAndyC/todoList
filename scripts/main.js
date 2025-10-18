caebb6de87416739f5296af3babc19edd23a826b
JoeAndyC <joelcuenca.jc@gmail.com>
2025-10-18 14:48:46 -0300

    Se inició con el JS, agregando la función para el botón añadir

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// Traer elementos del html
+const newTask = document.querySelector('.newTask');
+const addTaskButton =  document.querySelector('buttonAddTask');
+const taskList = document.querySelector('.taskList');
+
+
+while(true) {
+  try {
+    if (newTask.value !== '' || newTask.value !== undefined || newTask.value !== null) {
+      addTaskButton.addEventListener('clicl', => {
+        const task = document.createElement('div');
+        const pTask = document.createElement('p');
+        const deleteButton = document.createElement('button');
+        
+      })  
+    }
+  }
+}
