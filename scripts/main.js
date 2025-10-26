8ddd1b156bb3d94f212932533875474efbdfb07d
JoeAndyC <joelcuenca.jc@gmail.com>
2025-10-26 09:13:28 -0300

    Ya no se añade el div 'input' y se busca añadir el label con el input radio. Estado: no funciona

--- scripts/main.js
+++ scripts/main.js
@@ -12,15 +12,20 @@ addTaskButton.addEventListener('click', () => {
     return;
   }
   taskValue = newTask.value;
-  
+  //! Don´t show the new tasks 
   const task = document.createElement('div');
-  const radioModInput = document.createElement('div');
+  const radioModInputCreate = document.createElement('label');
+  const radioInput = document.createElement = 'input';
   const taskText = document.createElement('p');
   const deletTask = document.createElement('button');
-  const imgDelete = document.createElement('img')
+  const imgDelete = document.createElement('img');
   
   task.className = 'task';
-  radioModInput.className = 'radioModInput';
+  radioModInputCreate.className = 'radioModInput';
+  //radioModInput.for = 'done-task';
+  radioInput.className = 'radioInput';
+  radioInput.type = 'radio';
+ // radioInput.name = 'done-task';
   taskText.className = 'taskText';
   deletTask.className = 'deletTask';
   imgDelete.className = 'imagDelete';
@@ -32,7 +37,8 @@ addTaskButton.addEventListener('click', () => {
   imgDelete.alt = 'Imagen de Tacho de Basura';
 
   taskList.appendChild(task);
-  task.appendChild(radioModInput);
+  task.appendChild(radioModInputCreate);
+  task.appendChild(radioInput);
   task.appendChild(taskText);
   task.appendChild(deletTask);
   deletTask.appendChild(imgDelete);
@@ -41,10 +47,24 @@ addTaskButton.addEventListener('click', () => {
 });
 
 // Calling the radioModInput's to make a change when they are selected
+const radioModInput = document.querySelectorAll('.radioModInput');
 
+// Make the backgroundColor change color #1b93a1 || #f9f3d5
+radioModInput.forEach((input) => {
+  input.addEventListener('click', () => {
+    if (input.style.backgroundColor !== '#1b93a1') {
+      input.style.backgroundColor = '#1b93a1';
+    }
+  });
+});
 
+// Make a task change the text decoration
+const tasksList = document.querySelectorAll('.taskText');
 
-
-
+task.forEach((task) => {
+  task.addEventListener('click', () => {
+    task.style.textDecoration = 'line-through';
+  });
+});
 
 
