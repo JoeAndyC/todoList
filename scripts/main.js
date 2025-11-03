c5c071b6d38b8eb026cc29cd07023564be285dfe
JoeAndyC <joelcuenca.jc@gmail.com>
2025-11-03 14:19:23 -0300

    Se elimino el input y el label. Se elimino el archivo de styles y se elimino el código para seleccionar y para tachar las tareas

--- scripts/main.js
+++ scripts/main.js
@@ -11,21 +11,17 @@ addTaskButton.addEventListener('click', () => {
     errorMessage.textContent = 'Debes ingresar una tarea, no puedes ingresar un dato vacío!';
     return;
   }
+
   taskValue = newTask.value;
   //! Don´t show the new tasks 
   const task = document.createElement('div');
-  const radioModInputCreate = document.createElement('label');
-  const radioInput = document.createElement = 'input';
+  const editCheckBox = document.createElement('div');
   const taskText = document.createElement('p');
   const deletTask = document.createElement('button');
   const imgDelete = document.createElement('img');
   
   task.className = 'task';
-  radioModInputCreate.className = 'radioModInput';
-  //radioModInput.for = 'done-task';
-  radioInput.className = 'radioInput';
-  radioInput.type = 'radio';
- // radioInput.name = 'done-task';
+  editCheckBox.className = 'editCheckBox';
   taskText.className = 'taskText';
   deletTask.className = 'deletTask';
   imgDelete.className = 'imagDelete';
@@ -37,34 +33,12 @@ addTaskButton.addEventListener('click', () => {
   imgDelete.alt = 'Imagen de Tacho de Basura';
 
   taskList.appendChild(task);
-  task.appendChild(radioModInputCreate);
-  task.appendChild(radioInput);
+  task.appendChild(editCheckBox);
   task.appendChild(taskText);
   task.appendChild(deletTask);
   deletTask.appendChild(imgDelete);
   
   newTask.value = '';
-});
-
-// Calling the radioModInput's to make a change when they are selected
-const radioModInput = document.querySelectorAll('.radioModInput');
-
-// Make the backgroundColor change color #1b93a1 || #f9f3d5
-radioModInput.forEach((input) => {
-  input.addEventListener('click', () => {
-    if (input.style.backgroundColor !== '#1b93a1') {
-      input.style.backgroundColor = '#1b93a1';
-    }
-  });
-});
-
-// Make a task change the text decoration
-const tasksList = document.querySelectorAll('.taskText');
-
-task.forEach((task) => {
-  task.addEventListener('click', () => {
-    task.style.textDecoration = 'line-through';
-  });
 });
 
 
